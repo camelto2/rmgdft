20a2f46539f2bc3f08a54229737be538ef215070
camelto2 <cmelton@sandia.gov>
2022-03-15 16:55:13 -0600

    correct the ion-ion total energy for supercell generated by kpoints

--- XC/Exxbase.cpp
+++ XC/Exxbase.cpp
@@ -2346,7 +2346,9 @@ template <class T> void Exxbase<T>::write_basics(hid_t h_group, int_2d_array QKt
 
     std::vector<double> Energies;
 
-    Energies.push_back(ct.II);
+    //AFQMC E0 energy should be of supercell
+    //TO DO: Add e-e madelung energy of supercell to agree with real-space QMC total energy
+    Energies.push_back(ct.II * ct.klist.num_k_all);
     Energies.push_back(0.0);
     writeNumsToHDF("Energies", Energies, h_group);
 
